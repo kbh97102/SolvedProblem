69c1bb8e71eaf9d7bee7e8a9b06aff050d6d0869
Arakene <kbh97102@naver.com>
2019-09-20 20:34:18 +0900

    Sooooo ez

---
+++ No2742.java
@@ -0,0 +1,20 @@
+package homeStudy;
+
+import java.util.Scanner;
+
+public class No2742 {
+
+	public static void main(String[] args) {
+		Scanner scanf = new Scanner(System.in);
+		
+		int input = scanf.nextInt();
+		
+		
+		for(int i=input;i>=1;i--) {
+			System.out.println(i);
+		}
+
+		scanf.close();
+	}
+
+}
